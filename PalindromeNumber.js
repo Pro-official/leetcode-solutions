fa1eea366165d0bb2ae8cb0598c45455ae19da98
Pro-official <promise.official000@gmail.com>
2022-12-20 17:39:48 +0600

    roman to integer number is not yet solved

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -1,10 +1,20 @@
+// Given an integer x, return true if x is a palindrome, and false otherwise. Follow up: do it without converting the integers to strings.
+
 function isPalindrome(x) {
-  let xStr = x.toString();
-  return xStr === xStr.split("").reverse().join("");
+  if (x < 0) return false;
+
+  let reverse = 0;
+  let original = x;
+  while (x > 0) {
+    reverse = reverse * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  return reverse === original;
 }
 
 // Test cases
 console.log(isPalindrome(121)); // true
 console.log(isPalindrome(16461)); // true
 console.log(isPalindrome(-121)); // false
-console.log(isPalindrome(56665)); // false
+console.log(isPalindrome(56665)); // true
