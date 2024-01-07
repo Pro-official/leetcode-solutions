61502756496cf02324c5e56285561dae557aff5a
Pro-official <promise.official000@gmail.com>
2024-01-07 22:40:02 +0600

    Merge two sorted list solution

---
+++ mergeTwoSortedList.js
@@ -0,0 +1,60 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+// Example 1:
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2:
+// Input: list1 = [], list2 = []
+// Output: []
+
+// Example 3:
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+// Constraints:
+
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both list1 and list2 are sorted in non-decreasing order.
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+function mergeTwoLists(list1, list2) {
+  let dummy = new ListNode();
+  let current = dummy;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  // If one of the lists is not empty, append the remaining nodes
+  if (list1 !== null) {
+    current.next = list1;
+  }
+
+  if (list2 !== null) {
+    current.next = list2;
+  }
+
+  return dummy.next;
+}
+
+// Example usage:
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+const mergedList = mergeTwoLists(list1, list2);
+console.log(mergedList);
